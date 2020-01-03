f8bc575d27963d8cf7462489b119c48a694ad9b5
ferkki <ferkki@hotmail.com>
2020-01-03 15:45:51 -0400

    home terminado

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import '../assets/css/components/Card.css';
+import '../assets/css/components/Apod.css';
 
-const Apod = () => (
+const Apod = ({ copyright, title, url, hdurl, explanation }) => (
 	<div className='card'>
-		<div className='img'></div>
-		<div className='autor'></div>
-		<div className='title'></div>
-		<div className='desc'></div>
-		<div className='button-container'></div>
+		{copyright ? (
+			<>
+				<div
+					className='img'
+					style={{ backgroundImage: `url(${url})` }}></div>
+				<div className='autor'>
+					<span>by: {copyright}</span>
+				</div>
+				<div className='title'>
+					<span>{title}</span>
+				</div>
+				<div className='desc'>
+					<span>{explanation}</span>
+				</div>
+				<div className='button-container'>
+					<a
+						className='button-container-link'
+						href={hdurl}
+						target='_blank'
+						rel='noopener noreferrer'>
+						View HD image
+					</a>
+				</div>
+			</>
+		) : (
+			<div>
+				<h1>Loading...</h1>
+			</div>
+		)}
 	</div>
 );
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Apod from '../components/Apod';
+import { ApiKey } from '../constants/index';
 
 import '../assets/css/pages/Home.css';
 
+const URL = `https://api.nasa.gov/planetary/apod?api_key=${ApiKey}`;
+
 const Home = () => {
+	const [apod, setApod] = useState();
+	useEffect(() => {
+		axios.get(URL).then(resp => setApod(resp['data']));
+	}, []);
 	return (
 		<div>
 			<Header title='Space App' desc='See the space' />
 			<Navbar />
 			<section className='container'>
-				<Apod></Apod>
+				<Apod {...apod} />
 			</section>
 		</div>
 	);
