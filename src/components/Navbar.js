0984e7621ea71bb917337b5670ed4e04aae1a7d1
ferkki <ferkki@hotmail.com>
2020-01-03 10:41:22 -0400

    estructura inicial del header, navbar y la pagina de home

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+	return (
+		<nav>
+			<button>Home</button>
+			<button>Robert Mars</button>
+			<button>About</button>
+		</nav>
+	);
+};
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+	return (
+		<div>
+			<Header title='Space App' desc='See the space' />
+		</div>
+	);
+};
+
+export default Home;
