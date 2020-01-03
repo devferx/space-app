780528e0cfa1360e3671f967fc4968afd7695d94
ferkki <ferkki@hotmail.com>
2020-01-03 11:13:02 -0400

    container listo para insertar apod component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 
+import '../assets/css/pages/Home.css';
+
 const Home = () => {
 	return (
 		<div>
