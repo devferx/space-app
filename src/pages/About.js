73d2898e0690ea28c173584b8814244d2392befa
devfer <ferkki@hotmail.com>
2020-01-29 23:00:03 -0400

    contact page finalizada

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
+import '../assets/css/pages/about.css';
+
 const About = () => {
 	return (
-		<div>
-			<Layout title='About Us' desc='Contact me'>
-				<h1>Desde About</h1>
-			</Layout>
-		</div>
+		<Layout title='About Us' desc='Contact me'>
+			<div className='main-container'>
+				<div className='contact'>
+					<p>Contact</p>
+				</div>
+				<div className='container'>
+					<div className='caja'>
+						<h3>Fernando Quinteros G.</h3>
+						<p>
+							<strong>Email:</strong>
+						</p>
+						<p>fernandoquinterosgutierrez@gmail.com</p>
+						<p>
+							<strong>Phone:</strong>
+						</p>
+						<p>+591 75479004</p>
+					</div>
+				</div>
+			</div>
+		</Layout>
 	);
 };
 
