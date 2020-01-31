0918581ac640f8dcf4a70a266a84e1cd00c4084a
devfer <ferkki@hotmail.com>
2020-01-31 08:51:24 -0400

    Updates

---
+++ precache-manifest.188d84016ae570a6b7caa3d35e551a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5233878412c7579420d5fcdced53ec",
+    "url": "/space-app/index.html"
+  },
+  {
+    "revision": "d62b5af4ac2d1607c5aa",
+    "url": "/space-app/static/css/main.1c4d5848.chunk.css"
+  },
+  {
+    "revision": "d70629ec636a0686f3dd",
+    "url": "/space-app/static/js/2.1edf3b8d.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/space-app/static/js/2.1edf3b8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d62b5af4ac2d1607c5aa",
+    "url": "/space-app/static/js/main.78ef716c.chunk.js"
+  },
+  {
+    "revision": "ebd1b6b871959313be90",
+    "url": "/space-app/static/js/runtime-main.61c6df4b.js"
+  }
+]);
